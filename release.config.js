8ef9ee0d70081270e472a62d84437974cec293c3
Mirko Lenz <mirko@mirkolenz.com>
2023-01-06 10:56:30 +0100

    fix: run buf mod update before generation

--- release.config.js
+++ release.config.js
@@ -31,6 +31,7 @@ const config = {
       "@semantic-release/exec",
       {
         prepareCmd: [
+          "buf mod update",
           "buf generate --include-imports buf.build/recap/arg-services",
           "find ./arg_services -type d -exec touch {}/__init__.py \\;",
           "cp -f arg_services_init.py arg_services/__init__.py",
@@ -58,7 +59,7 @@ const config = {
       "@semantic-release/git",
       {
         message: "chore(release): ${nextRelease.version}",
-        assets: ["pyproject.toml", "*/__init__.py", "CHANGELOG.md"],
+        assets: ["pyproject.toml", "*/__init__.py", "CHANGELOG.md", "buf.lock"],
       },
     ],
   ],
