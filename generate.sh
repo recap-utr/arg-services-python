3e9355fb5f4ff6976a15801cc586bac26fbafa7a
Mirko Lenz <mirko@mirkolenz.com>
2023-01-15 11:55:36 +0100

    fix: init file was copied to wrong folder

--- generate.sh
+++ generate.sh
@@ -4,4 +4,4 @@ buf mod update &&
     buf generate --include-imports buf.build/recap/arg-services &&
     find src -type d -exec touch {}/__init__.py \; &&
     rm -f src/__init__.py &&
-    cp -rf arg_services/ src/arg_services/
+    cp -rf arg_services/__init__.py src/arg_services/__init__.py
